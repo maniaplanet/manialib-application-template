5d3c3d4f37f6ffb3962277a2398668de40ba914e
gou1 <maximeraoust@gmail.com>
2009-06-29 13:27:44 +0000

    git-svn-id: http://manialib.googlecode.com/svn/trunk@64 e79368ca-600c-11de-ad4a-3d330faba797

---
+++ manialib-example/config-example.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Example config file. Rename to config.php
+ * 
+ * @author Maxime Raoust
+ */
+
+define("APP_URL", "http://localhost/manialib/");
+
+define("MANIALINK_NAME", "manialibdev");
+
+// If you use the short manialinks option, rename "use_short_manialinks.htaccess" to .htaccess
+// and check that Apache mod rewrite is enabled on your server
+define("USE_SHORT_MANIALINKS", true); 
+
+define("DEFAULT_TIMEZONE", "Europe/Paris");
+
+define("DATABASE_HOST", "localhost");
+define("DATABASE_USER", "root");
+define("DATABASE_PASSWORD", "");
+define("DATABASE_NAME", "manialib");
+
+?>
---
+++ manialib-example/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Maxime Raoust
+ */
+
+ 
+require_once("core.inc.php");
+
+
+//trigger_error(print_r($_GET, true));
+
+$request = RequestEngine::getInstance();
+
+require_once("header.php");
+
+require_once("navigation.php");
+
+
+
+Manialink::beginFrame(-34, 48, 1);
+	
+	Manialink::beginFrame(25, -15, 1);
+		
+		PostsEngine::getInstance();
+		if($request->get("post_id")) require_once( APP_LIBRARIES_PATH . "posts/views/show_post.php");
+		else                    require_once( APP_LIBRARIES_PATH . "posts/views/last_posts.php");
+
+	Manialink::endFrame();
+
+Manialink::endFrame();
+
+require_once("footer.php");
+
+
+?>
