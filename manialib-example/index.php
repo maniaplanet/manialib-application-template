ee20fbf9ddbe90c55ce7be54ca7a68836383454a
gou1 <maximeraoust@gmail.com>
2010-03-12 16:39:09 +0000

    This revision may not be stable. You can use the latest tag instead.
    git-svn-id: http://manialib.googlecode.com/svn/trunk@210 e79368ca-600c-11de-ad4a-3d330faba797

--- manialib-example/index.php
+++ manialib-example/index.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * This file is part of ManiaLib.
- * 
- * ManiaLib is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *  
+ * ManiaLib  is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  * 
  * ManiaLib is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
