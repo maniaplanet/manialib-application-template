3967ec58cd8dc22829a127da686ed0a4b7f9c0f9
gou1 <maximeraoust@gmail.com>
2010-10-07 11:02:27 +0000

    MVC example: navigation menu updated
    git-svn-id: http://manialib.googlecode.com/svn/trunk@347 e79368ca-600c-11de-ad4a-3d330faba797

--- examples/mvc/views/home/_navigation.php
+++ examples/mvc/views/home/_navigation.php
@@ -7,108 +7,61 @@
 $request = RequestEngineMVC::getInstance();
 $response = ResponseEngine::getInstance();
 
-Manialink::beginFrame(-60, 48, 1);
-{
-	Manialink::beginFrame(0, 0, 0);
-	{
-		$ui = new Quad(35, 25);
-		$ui->setPosition(0, 5);
-		$ui->setSubStyle(Bgs1::NavButtonBlink);
-		$ui->save();
-		
-		Manialink::beginFrame(2, -8, 1);
-		{
-			$ui = new Quad(9, 9);
-			$ui->setImage('logo64.dds');
-			$ui->save();
-			
-			$ui = new Label(50);
-			$ui->setPosition(9, -1.75, 0);
-			$ui->setStyle(Label::TextRankingsBig);
-			$ui->setText(__('ManiaLib'));
-			$ui->save();
-			
-			$ui = new Label(50);
-			$ui->setPosition(9, -5.25, 0);
-			$ui->setStyle(Label::TextInfoSmall);
-			$ui->setText(__('empower_your_manialinks'));
-			$ui->save();
-		}
-		Manialink::endFrame();
-	}
-	Manialink::endFrame();
-	
-	Manialink::beginFrame(0, -21, 0);
-	{
-		$layout = new ColumnLayout();
-		$layout->setMarginHeight(1);
-		
-		Manialink::beginFrame(0, 0, 0, $layout);
-		{
-			$manialink = $request->createLink(Route::CUR, Route::NONE);
-			$selected = $request->getAction('about') == 'about';
-			
-			$ui = new NavigationButton(35, 8);
-			if($selected) $ui->setSelected();
-			$ui->setManialink($manialink);
-			$ui->icon->setSubStyle(Icons128x128_1::Manialink);
-			$ui->text->setText(__('about'));
-			$ui->save();
-			
-			$manialink = $request->createLink(Route::CUR, 'features');
-			$selected = $request->getAction() == 'features';
-			
-			$ui = new NavigationButton(35, 8);
-			if($selected) $ui->setSelected();
-			$ui->setManialink($manialink);
-			$ui->icon->setSubStyle(Icons128x128_1::Forever);
-			$ui->text->setText(__('features'));
-			$ui->save();
-			
-			$manialink = $request->createLink(Route::CUR, 'download');
-			$selected = $request->getAction() == 'download';
-			
-			$ui = new NavigationButton(35, 8);
-			if($selected) $ui->setSelected();
-			$ui->setManialink($manialink);
-			$ui->icon->setSubStyle(Icons128x128_1::Load);
-			$ui->text->setText(__('download'));
-			$ui->save();
-			
-			$manialink = $request->createLink(Route::CUR, 'showcase');
-			$selected = $request->getAction() == 'showcase';
-			
-			$ui = new NavigationButton(35, 8);
-			if($selected) $ui->setSelected();
-			$ui->setManialink($manialink);
-			$ui->icon->setSubStyle(Icons128x128_1::ServersSuggested);
-			$ui->text->setText(__('showcase'));
-			$ui->save();
-			
-			$manialink = $request->createLink('examples', Route::NONE);
-			
-			$ui = new NavigationButton(35, 8);
-			$ui->setManialink($manialink);
-			$ui->icon->setSubStyle(Icons128x128_1::Browse);
-			$ui->text->setText(__('sample_pages'));
-			$ui->save();
-			
-			$manialink = $request->createLink('shoutbox', Route::NONE);
-			
-			$ui = new NavigationButton(35, 8);
-			$ui->setManialink($manialink);
-			$ui->icon->setSubStyle(Icons128x128_1::Share);
-			$ui->text->setText(__('shoutbox'));
-			$ui->save();
-			
-			$ui = new Quad(35, 45);
-			$ui->setSubStyle(Bgs1::NavButtonBlink);
-			$ui->save();
-		}
-		Manialink::endFrame();
-	}
-	Manialink::endFrame();
-}
-Manialink::endFrame();
+$ui = new Navigation();
+$ui->logo->setImage('logo64.dds');
+$ui->title->setText(__('ManiaLib'));
+$ui->subTitle->setText(__('empower_your_manialinks'));
+
+$manialink = $request->createLink(Route::CUR, Route::NONE);
+$selected = $request->getAction('about') == 'about';
+
+$ui->addItem();
+$ui->lastItem->setManialink($manialink);
+if($selected) $ui->lastItem->setSelected();
+$ui->lastItem->icon->setSubStyle(Icons128x128_1::Manialink);
+$ui->lastItem->text->setText(__('about'));
+
+$manialink = $request->createLink(Route::CUR, 'features');
+$selected = $request->getAction() == 'features';
+
+$ui->addItem();
+$ui->lastItem->setManialink($manialink);
+if($selected) $ui->lastItem->setSelected();
+$ui->lastItem->icon->setSubStyle(Icons128x128_1::Forever);
+$ui->lastItem->text->setText(__('features'));
+
+$manialink = $request->createLink(Route::CUR, 'download');
+$selected = $request->getAction() == 'download';
+
+$ui->addItem();
+$ui->lastItem->setManialink($manialink);
+if($selected) $ui->lastItem->setSelected();
+$ui->lastItem->icon->setSubStyle(Icons128x128_1::Load);
+$ui->lastItem->text->setText(__('download'));
+
+$manialink = $request->createLink(Route::CUR, 'showcase');
+$selected = $request->getAction() == 'showcase';
+
+$ui->addItem();
+$ui->lastItem->setManialink($manialink);
+if($selected) $ui->lastItem->setSelected();
+$ui->lastItem->icon->setSubStyle(Icons128x128_1::ServersSuggested);
+$ui->lastItem->text->setText(__('showcase'));
+
+$manialink = $request->createLink('examples', Route::NONE);
+
+$ui->addItem();
+$ui->lastItem->setManialink($manialink);
+$ui->lastItem->icon->setSubStyle(Icons128x128_1::Browse);
+$ui->lastItem->text->setText(__('sample_pages'));
+
+$manialink = $request->createLink('shoutbox', Route::NONE);
+
+$ui->addItem();
+$ui->lastItem->setManialink($manialink);
+$ui->lastItem->icon->setSubStyle(Icons128x128_1::Share);
+$ui->lastItem->text->setText(__('shoutbox'));
+
+$ui->save();
 
 ?>
--- examples/mvc/views/shoutbox/_navigation.php
+++ examples/mvc/views/shoutbox/_navigation.php
@@ -7,71 +7,23 @@
 $request = RequestEngineMVC::getInstance();
 $response = ResponseEngine::getInstance();
 
-Manialink::beginFrame(-60, 48, 1);
-{
-	Manialink::beginFrame(0, 0, 0);
-	{
-		$ui = new Quad(35, 25);
-		$ui->setPosition(0, 5);
-		$ui->setSubStyle(Bgs1::NavButtonBlink);
-		$ui->save();
-		
-		Manialink::beginFrame(2, -8, 1);
-		{
-			$ui = new Quad(9, 9);
-			$ui->setStyle(Quad::Icons128x128_1);
-			$ui->setSubStyle(Icons128x128_1::Share);
-			$ui->save();
-			
-			$ui = new Label(50);
-			$ui->setPosition(9, -1.75, 0);
-			$ui->setStyle(Label::TextRankingsBig);
-			$ui->setText(__('shoutbox'));
-			$ui->save();
-			
-			$ui = new Label(50);
-			$ui->setPosition(9, -5.25, 0);
-			$ui->setStyle(Label::TextInfoSmall);
-			$ui->setText(__('shoutbox_example'));
-			$ui->save();
-		}
-		Manialink::endFrame();
-	}
-	Manialink::endFrame();
-	
-	Manialink::beginFrame(0, -21, 0);
-	{
-		$layout = new ColumnLayout();
-		$layout->setMarginHeight(1);
-		
-		Manialink::beginFrame(0, 0, 0, $layout);
-		{
-			$manialink = $request->createLink(Route::CUR, Route::NONE);
-			$selected = $request->getAction('view_shouts') == 'view_shouts';
-			
-			$ui = new NavigationButton(35, 8);
-			if($selected) $ui->setSelected();
-			$ui->setManialink($manialink);
-			$ui->icon->setSubStyle(Icons128x128_1::Share);
-			$ui->text->setText(__('view_shouts'));
-			$ui->save();
-			
-			$manialink = $request->createLink(Route::DEF, Route::NONE);
-			
-			$ui = new NavigationButton(35, 8);
-			$ui->setManialink($manialink);
-			$ui->icon->setSubStyle(Icons128x128_1::Back);
-			$ui->text->setText('Back');
-			$ui->save();
-			
-			$ui = new Quad(35, 70);
-			$ui->setSubStyle(Bgs1::NavButtonBlink);
-			$ui->save();
-		}
-		Manialink::endFrame();
-	}
-	Manialink::endFrame();
-}
-Manialink::endFrame();
+$ui = new Navigation();
+$ui->logo->setSubStyle(Icons128x128_1::Share);
+$ui->title->setText(__('shoutbox'));
+$ui->subTitle->setText(__('shoutbox_example'));
+
+$manialink = $request->createLink(Route::CUR, Route::NONE);
+$selected = $request->getAction('view_shouts') == 'view_shouts';
+
+$ui->addItem();
+$ui->lastItem->setManialink($manialink);
+if($selected) $ui->lastItem->setSelected();
+$ui->lastItem->icon->setSubStyle(Icons128x128_1::Share);
+$ui->lastItem->text->setText(__('view_shouts'));
+
+$manialink = $request->createLink(Route::DEF, Route::NONE);
+
+$ui->quitButton->setManialink($manialink);
+$ui->save();
 
 ?>
