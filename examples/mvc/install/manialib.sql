93d316d9e9274a5ed3c7e61f60a4e37c2a214934
gou1 <maximeraoust@gmail.com>
2010-11-08 16:21:16 +0000

    blabla
    git-svn-id: http://manialib.googlecode.com/svn/trunk@452 e79368ca-600c-11de-ad4a-3d330faba797

--- examples/mvc/install/manialib.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `ManiaLib_Shouts` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `datePosted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `login` varchar(25) NOT NULL,
-  `nickname` varchar(75) NOT NULL,
-  `message` varchar(160) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
