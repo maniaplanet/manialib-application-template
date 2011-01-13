c86649d889a068e4a6a7947c62aecb2ccb653430
gou1 <maximeraoust@gmail.com>
2011-01-13 17:09:25 +0000

    Updated autoload
    git-svn-id: http://manialib.googlecode.com/svn/trunk@486 e79368ca-600c-11de-ad4a-3d330faba797

--- libraries/autoload.php
+++ libraries/autoload.php
@@ -16,46 +16,16 @@ if(!defined('APP_PATH'))
 }
 
 define('NAMESPACE_SEPARATOR', '\\');
-define('MANIASTUDIO_LIBRARIES_PATH', dirname(__FILE__));
+
+define('APP_LIBRARIES_PATH', __DIR__);
 
 function __autoload($className)
 {
 	$className = str_replace(NAMESPACE_SEPARATOR, DIRECTORY_SEPARATOR, $className);
-	$path = MANIASTUDIO_LIBRARIES_PATH.DIRECTORY_SEPARATOR.$className.'.php';
+	$path = APP_LIBRARIES_PATH.DIRECTORY_SEPARATOR.$className.'.php';
 	if(file_exists($path))
 	{
 		require_once $path;
-	}
-}
-
-function require_once_recursive($directory)
-{
-	if ($handle = opendir($directory))
-	{
-		while (false !== ($file = readdir($handle)))
-		{
-			if(substr($file, 0, 1)==".")
-			{
-				continue;
-			}
-			if(is_dir($directory.'/'.$file))
-			{
-				require_once_recursive($directory.'/'.$file);
-			}
-			else
-			{
-				require_once $directory.'/'.$file;
-			}
-		}
-		closedir($handle);
-	}
-}
-
-if(!function_exists('_'))
-{
-	function _($text)
-	{
-		return $text;
 	}
 }
 
