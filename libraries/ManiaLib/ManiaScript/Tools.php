34310fac3930898a823e4659dbf623a7162a0cb6
gou1 <maximeraoust@gmail.com>
2011-10-25 14:09:54 +0000

    Added ManiaLib\ManiaScript package with some escaping tools
    git-svn-id: http://manialib.googlecode.com/svn/trunk@520 e79368ca-600c-11de-ad4a-3d330faba797

---
+++ libraries/ManiaLib/ManiaScript/Tools.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * ManiaLib - Lightweight PHP framework for Manialinks
+ * 
+ * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
+ */
+
+namespace ManiaScript;
+
+/**
+ * ManiaScript tools
+ */
+abstract class Tools
+{
+
+	/**
+	 * Returns a escaped and quoted string so you can use it a func param.
+	 * Eg. if you pass ubisoft"nadeo il will return "ubisoft\"nadeo"
+	 * @return string
+	 */
+	static function quote($string)
+	{
+		return sprintf('"%s"', self::escapeString($string));
+	}
+
+	static function escapeString($string)
+	{
+		return addcslashes($string, '"');
+	}
+
+}
+
+?>
