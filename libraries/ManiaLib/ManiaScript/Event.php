223311cc0c277cb5ffa11659dac460385674b6d8
gou1 <maximeraoust@gmail.com>
2011-11-25 17:52:42 +0000

    Added ManiaLib\ManiaScript\Event for easy encapsulation of "manialib_event" related stuff
    git-svn-id: http://manialib.googlecode.com/svn/trunk@554 e79368ca-600c-11de-ad4a-3d330faba797

---
+++ libraries/ManiaLib/ManiaScript/Event.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * ManiaLib - Lightweight PHP framework for Manialinks
+ * 
+ * @see         http://code.google.com/p/manialib/
+ * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
+ */
+
+namespace ManiaLib\ManiaScript;
+
+use ManiaLib\Gui\Manialink;
+
+abstract class Event
+{
+	const mouseClick = 'CGameManialinkScriptEvent::Type::MouseClick';
+	const mouseOver = 'CGameManialinkScriptEvent::Type::MouseOver';
+	const mouseOut = 'CGameManialinkScriptEvent::Type::MouseOut';
+
+	static function addListener($controlId, $eventType, array $action)
+	{
+		$script = 'manialib_event_add_listener("%s", %s, %s); ';
+		$controlId = Tools::escapeString($controlId);
+		$action = Tools::array2maniascript($action);
+		$script = sprintf($script, $controlId, $eventType, $action);
+		Manialink::appendScript($script);
+	}
+
+}
+
+?>
