5d110e6f5d6d7a68661359472c1ff0051ff37d56
gou1 <maximeraoust@gmail.com>
2011-11-25 17:15:29 +0000

    Added bindings for the NEW VERSION "manialib2.xml" in ManiaLib\ManiaScript\UI ; this class is a mess for now, clean will be done when manialib.xml is removed from the svn...
    git-svn-id: http://manialib.googlecode.com/svn/trunk@552 e79368ca-600c-11de-ad4a-3d330faba797

--- libraries/ManiaLib/ManiaScript/UI.php
+++ libraries/ManiaLib/ManiaScript/UI.php
@@ -26,6 +26,8 @@ abstract class UI
 {
 
 	/**
+	 * ATCHUNG: FOR "manialib.xml" ie. FIRST VERSION !
+	 * 
 	 * Teh infamous dialog box
 	 * 
 	 * @param string $openControlId Id of the element that will open the dialog when clicked
@@ -45,6 +47,8 @@ abstract class UI
 	}
 
 	/**
+	 * ATCHUNG: FOR "manialib.xml" ie. FIRST VERSION !
+	 * 
 	 * Nice little tooltip when mousing over
 	 * 
 	 * @param string $controlId Id of the element that will be tooltiped
@@ -53,6 +57,43 @@ abstract class UI
 	static function autotip2($controlId, $message)
 	{
 		$script = 'manialib_ui_autotip2("%s", "%s"); ';
+		$controlId = Tools::escapeString($controlId);
+		$message = Tools::escapeString($message);
+		$script = sprintf($script, $controlId, $message);
+		Manialink::appendScript($script);
+	}
+	
+	
+	/**
+	 * ATCHUNG: FOR "manialib2.xml" ie. SECOND VERSION !
+	 * 
+	 * Teh infamous dialog box
+	 * 
+	 * @param string $openControlId Id of the element that will open the dialog when clicked
+	 * @param string $message Message to show in the dialog
+	 * @param array $action A ManiaScript Framework Action
+	 */
+	static function dialog($openControlId, $message, array $action)
+	{
+		$script = 'manialib_ui_dialog("%s", "%s", %s); ';
+		$openControlId = Tools::escapeString($openControlId);
+		$message = Tools::escapeString($message);
+		$action = Tools::array2maniascript($action);
+		$script = sprintf($script, $openControlId, $message, $action);
+		Manialink::appendScript($script);
+	}
+	
+	/**
+	 * ATCHUNG: FOR "manialib2.xml" ie. SECOND VERSION !
+	 * 
+	 * Nice little tooltip when mousing over
+	 * 
+	 * @param string $controlId Id of the element that will be tooltiped
+	 * @param string $message
+	 */
+	static function tooltip($controlId, $message)
+	{
+		$script = 'manialib_ui_tooltip("%s", "%s"); ';
 		$controlId = Tools::escapeString($controlId);
 		$message = Tools::escapeString($message);
 		$script = sprintf($script, $controlId, $message);
