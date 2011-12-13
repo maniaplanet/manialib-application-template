85f0461ecdf97605c2d501a40d778a98d56038c4
gou1 <maximeraoust@gmail.com>
2011-12-13 12:25:41 +0000

    ManiaScript+ManiaLib usage example
    git-svn-id: http://manialib.googlecode.com/svn/trunk@566 e79368ca-600c-11de-ad4a-3d330faba797

--- libraries/ManiaLibDemo/Views/ManiaScript/Index.php
+++ libraries/ManiaLibDemo/Views/ManiaScript/Index.php
@@ -22,15 +22,29 @@ class Index extends \ManiaLib\Application\View
 		
 		Manialink::appendScript('main() {');
 		
-		$ui = new \ManiaLib\Gui\Elements\Button();
-		$ui->setAlign('center', 'center');
-		$ui->setText('Go to ManiaHome');
-		$ui->setId('maniahome-button');
-		$ui->setScriptEvents();
-		$ui->save();
-		
-		UI::dialog('maniahome-button', 'Do you want to visit ManiaHome?', array(Action::manialink, "maniahome"));
-		UI::tooltip('maniahome-button', 'Click me! Click me!');
+		Manialink::beginFrame(0, 0, 0, 1, new \ManiaLib\Gui\Layouts\Column());
+		{
+			$ui = new \ManiaLib\Gui\Elements\Button();
+			$ui->setAlign('center', 'center');
+			$ui->setText('Go to ManiaHome');
+			$ui->setId('maniahome-button');
+			$ui->setScriptEvents();
+			$ui->save();
+
+			UI::dialog('maniahome-button', 'Do you want to visit ManiaHome?', array(Action::manialink, "maniahome"));
+			UI::tooltip('maniahome-button', 'Click me! Click me!');
+			
+			$ui = new \ManiaLib\Gui\Elements\Button();
+			$ui->setAlign('center', 'center');
+			$ui->setText('Go to ManiaLoto');
+			$ui->setId('manialoto-button');
+			$ui->setScriptEvents();
+			$ui->save();
+
+			UI::dialog('manialoto-button', 'Do you want to visit ManiaLo\to?'."\n".'Hmmm kay?', array(Action::manialink, "manialoto"));
+			UI::tooltip('manialoto-button', 'Click me too!');
+		}
+		Manialink::endFrame();
 		
 		Manialink::appendScript('manialib_main_loop();');
 		Manialink::appendScript('}');
