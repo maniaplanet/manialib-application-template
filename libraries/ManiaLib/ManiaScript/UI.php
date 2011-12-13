5a81c7d92856160a22ee0d811e2cb4e82cb39716
gou1 <maximeraoust@gmail.com>
2011-12-13 12:24:26 +0000

    Deleted old manialib.xml, removed unused methods from ManiaLib\ManiaScript\UI
    git-svn-id: http://manialib.googlecode.com/svn/trunk@565 e79368ca-600c-11de-ad4a-3d330faba797

--- libraries/ManiaLib/ManiaScript/UI.php
+++ libraries/ManiaLib/ManiaScript/UI.php
@@ -24,49 +24,8 @@ use ManiaLib\Gui\Manialink;
  */
 abstract class UI
 {
-
-	/**
-	 * ATCHUNG: FOR "manialib.xml" ie. FIRST VERSION !
-	 * 
-	 * Teh infamous dialog box
-	 * 
-	 * @param string $openControlId Id of the element that will open the dialog when clicked
-	 * @param string $message Message to show in the dialog
-	 * @param string $actionName Action: manialink, maniazone, external, none
-	 * @param string $actionValue
-	 */
-	static function dialog2($openControlId, $message, $actionName, $actionValue)
-	{
-		$script = 'manialib_ui_dialog2("%s", "%s", "%s", "%s"); ';
-		$openControlId = Tools::escapeString($openControlId);
-		$message = Tools::escapeString($message);
-		$actionName = Tools::escapeString($actionName);
-		$actionValue = Tools::escapeString($actionValue);
-		$script = sprintf($script, $openControlId, $message, $actionName, $actionValue);
-		Manialink::appendScript($script);
-	}
-
-	/**
-	 * ATCHUNG: FOR "manialib.xml" ie. FIRST VERSION !
-	 * 
-	 * Nice little tooltip when mousing over
-	 * 
-	 * @param string $controlId Id of the element that will be tooltiped
-	 * @param string $message
-	 */
-	static function autotip2($controlId, $message)
-	{
-		$script = 'manialib_ui_autotip2("%s", "%s"); ';
-		$controlId = Tools::escapeString($controlId);
-		$message = Tools::escapeString($message);
-		$script = sprintf($script, $controlId, $message);
-		Manialink::appendScript($script);
-	}
-	
 	
 	/**
-	 * ATCHUNG: FOR "manialib2.xml" ie. SECOND VERSION !
-	 * 
 	 * Teh infamous dialog box
 	 * 
 	 * @param string $openControlId Id of the element that will open the dialog when clicked
@@ -84,8 +43,6 @@ abstract class UI
 	}
 	
 	/**
-	 * ATCHUNG: FOR "manialib2.xml" ie. SECOND VERSION !
-	 * 
 	 * Nice little tooltip when mousing over
 	 * 
 	 * @param string $controlId Id of the element that will be tooltiped
