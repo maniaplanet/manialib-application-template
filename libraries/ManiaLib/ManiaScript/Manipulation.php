b1c48d367ffb3433d10100c04e7765897f6bd129
gou1 <maximeraoust@gmail.com>
2011-12-22 14:38:10 +0000

    Added ManiaLib\ManiaScript\Manipulation to have (the begining of) php encapsulation of manipulation methods (hide, show, toggle, etc.)
    git-svn-id: http://manialib.googlecode.com/svn/trunk@587 e79368ca-600c-11de-ad4a-3d330faba797

---
+++ libraries/ManiaLib/ManiaScript/Manipulation.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * ManiaLib - Lightweight PHP framework for Manialinks
+ * 
+ * @see         http://code.google.com/p/manialib/
+ * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
+ * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version     $Revision$:
+ * @author      $Author$:
+ * @date        $Date$:
+ */
+
+namespace ManiaLib\ManiaScript;
+
+use ManiaLib\Gui\Manialink;
+
+/**
+ * This is a BETA PHP abstraction of the ManiaScript framework, so the warning 
+ * in the latter still applies:
+ * 
+ * This framework is not finished, not documented, not very useful and quite 
+ * possibly full of bugs. We'll also probably break everything at some point to
+ * refactor the code. However it's LGPL, so you can use at your own risk :)
+ */
+abstract class Manipulation
+{
+	static function hide($controlId)
+	{
+		$script = 'manialib_hide("%s"); ';
+		$controlId = Tools::escapeString($controlId);
+		$script = sprintf($script, $controlId);
+		Manialink::appendScript($script);
+	}
+	
+	static function show($controlId)
+	{
+		$script = 'manialib_hide("%s"); ';
+		$controlId = Tools::escapeString($controlId);
+		$script = sprintf($script, $controlId);
+		Manialink::appendScript($script);
+	}
+	
+	static function toggle($controlId)
+	{
+		$script = 'manialib_hide("%s"); ';
+		$controlId = Tools::escapeString($controlId);
+		$script = sprintf($script, $controlId);
+		Manialink::appendScript($script);
+	}
+}
+
+?>
