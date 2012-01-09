4490307012bf62c190d2ae3911a6ccdb6b3b19b9
gou1 <maximeraoust@gmail.com>
2012-01-09 11:36:19 +0000

    Cleanup in ManiaLib\Utils\TMStrings
    git-svn-id: http://manialib.googlecode.com/svn/trunk@618 e79368ca-600c-11de-ad4a-3d330faba797

--- libraries/ManiaLib/Utils/TMStrings.php
+++ libraries/ManiaLib/Utils/TMStrings.php
@@ -16,36 +16,15 @@ abstract class TMStrings
 {
 
 	/**
-	 * Allows to safely put any TM-formatted string into another TM-formatted string
-	 * without conflicts (conflict example: you put a nickname in the middle of the
-	 * sentance, the nickname has some bold characters and all the end of the
-	 * sentance becomes bold)
-	 * @param string Unprotected string
-	 * @param string Protected string
-	 */
-	static function protectStyles($string)
-	{
-		return '$<'.$string.'$>';
-	}
-
-	/**
-	 * Removes some TM styles (wide, bold and shadowed) to avoid wide words
-	 * @param string
-	 * @return string
+	 * Removes wide, bold and shadowed
 	 */
 	static function stripWideFonts($string)
 	{
-		return str_ireplace(array(
-				'$w',
-				'$o',
-				'$s'
-				), "", $string);
+		return str_ireplace(array('$w', '$o', '$s'), "", $string);
 	}
 
 	/**
-	 * Removes TM links
-	 * @param string
-	 * @return string
+	 * Removes links
 	 */
 	static function stripLinks($string)
 	{
@@ -54,103 +33,25 @@ abstract class TMStrings
 	}
 
 	/**
-	 * Removes all color in string
-	 * @param string $str
-	 * @return string
+	 * Removes colors
 	 */
 	static function stripColors($string)
 	{
-		return preg_replace('/\\$([tinmgz]|[0-9a-fA-F]{3}|[0-9a-fA-F].{2}|[0-9a-fA-F].[0-9a-fA-F]|[0-9a-fA-F]{2}.|[^$hlpwos<>]?)/iu',
+		return preg_replace(
+				'/\\$([tinmgz]|[0-9a-fA-F]{3}|[0-9a-fA-F].{2}|[0-9a-fA-F].[0-9a-fA-F]|[0-9a-fA-F]{2}.|[^$hlpwos<>]?)/iu',
 				"", $string);
 	}
 
 	/**
-	 * Removes the protecting styles ($< and $>) from a string
-	 * @param string Protected string
-	 * @return string Unprotected string
-	 */
-	static function unprotectStyles($string)
-	{
-		return preg_replace('/([^\$])(\$>|\$<)/u', '$1', $string);
-	}
-
-	/**
-	 * Removes all label formating from the string
-	 * @param string $string
-	 * @return string
+	 * Removes all styles
 	 */
 	static function stripAllTmStyle($string)
 	{
-		$string = self::unprotectStyles($string);
+		$string = preg_replace('/([^\$])(\$>|\$<)/u', '$1', $string);
 		$string = self::stripLinks($string);
 		$string = self::stripWideFonts($string);
-		return self::stripColors($string);
-	}
-
-	/**
-	 * Format a line with [yellow bold] title + content label
-	 * eg: "<b>Title:</b>   content"
-	 */
-	static function formatLine($title, $label='', $titleStyle = '$o$ff0',
-		$labelStyle = '')
-	{
-		return ($title ? '$<'.$titleStyle.$title.'$<$n $>:$>' : '').($label ? '    '.$labelStyle.$label
-					: '');
-	}
-
-	/**
-	 * 350 Coppers or N/A
-	 */
-	static function formatCoppersAmount($amount, $strict = true)
-	{
-		if(!$amount && $strict)
-		{
-			return _('N/A');
-		}
-		else
-		{
-			return sprintf(_('%s Coppers'), $amount);
-		}
-	}
-
-	static function formatDate($timestamp)
-	{
-		return date('l jS \of F Y', $timestamp);
-	}
-
-	static function formatLongDate($timestamp)
-	{
-		//return date('l jS \of F Y @ h:i A e', $timestamp);
-		return date('r', $timestamp);
-	}
-
-	static function formatRank($rank, $fullForPodium = true)
-	{
-		if($fullForPodium)
-		{
-			switch($rank)
-			{
-				case 1:
-					return 'first';
-				case 2:
-					return 'second';
-				case 3:
-					return 'third';
-			}
-		}
-
-		if(intval(($rank % 100) / 10) != 1)
-		{
-			if($rank % 10 == 1)
-				$suffix = 'st';
-			else if($rank % 10 == 2)
-				$suffix = 'nd';
-			else if($rank % 10 == 3)
-				$suffix = 'rd';
-		}
-		if(!isset($suffix))
-			$suffix = 'th';
-		return $rank.$suffix;
+		$string = self::stripColors($string);
+		return $string;
 	}
 
 }
