562b14825a122fb79a945f07f668bbe380fda09f
gou1 <maximeraoust@gmail.com>
2012-02-01 16:24:00 +0000

    Fixed file header, removed trailing space
    git-svn-id: http://manialib.googlecode.com/svn/trunk@656 e79368ca-600c-11de-ad4a-3d330faba797

--- libraries/ManiaLib/Utils/StyleParser.php
+++ libraries/ManiaLib/Utils/StyleParser.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * @copyright   Copyright (c) 2009-2012 NADEO (http://www.nadeo.com)
+ * ManiaLib - Lightweight PHP framework for Manialinks
+ * 
+ * @see         http://code.google.com/p/manialib/
+ * @copyright   Copyright (c) 2009-2011 NADEO (http://www.nadeo.com)
  * @license     http://www.gnu.org/licenses/lgpl.html LGPL License 3
  * @version     $Revision$:
  * @author      $Author$:
