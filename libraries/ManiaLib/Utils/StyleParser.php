38e106fafcfa38e83f0d6363be86ea355a5ee6c0
Martin Gwendal <martin.gwendal@gmail.com>
2012-02-29 19:43:29 +0000

    Bug fixed: StyleParser didn't stop links on '$>' when they were started after the corresponding '$<'
    git-svn-id: http://manialib.googlecode.com/svn/trunk@673 e79368ca-600c-11de-ad4a-3d330faba797

--- libraries/ManiaLib/Utils/StyleParser.php
+++ libraries/ManiaLib/Utils/StyleParser.php
@@ -23,7 +23,6 @@ class StyleParser
 	const SHADOWED = 0x8000;
 	const WIDE = 0x10000;
 	const NARROW = 0x20000;
-
 	const CONTRAST_DARKER = -1;
 	const CONTRAST_AUTO = 0;
 	const CONTRAST_LIGHTER = 1;
@@ -96,15 +95,13 @@ class StyleParser
 	/**
 	 * Declare a font to use with onImage function
 	 */
-	static function declareFont($name, $normal, $bold=null, $italic=null,
-		$boldItalic=null)
+	static function declareFont($name, $normal, $bold = null, $italic = null, $boldItalic = null)
 	{
 		$this->fonts[$name] = array(
 			0 => $normal,
 			StyleParser::BOLD => $bold ? : $normal,
 			StyleParser::ITALIC => $italic ? : $normal,
-			StyleParser::BOLD | StyleParser::ITALIC => $boldItalic ? : ($bold ? : ($italic ?
-							: $normal))
+			StyleParser::BOLD | StyleParser::ITALIC => $boldItalic ? : ($bold ? : ($italic ? : $normal))
 		);
 	}
 
@@ -133,18 +130,14 @@ class StyleParser
 	 * @param int $size	Font size
 	 * @param string $defaultColor Default text color in 3 or 6 hexadecimal characters
 	 */
-	static function onImage($string, $image, $fontName, $x=0, $y=0, $size=10,
-		$defaultColor='000')
+	static function onImage($string, $image, $fontName, $x = 0, $y = 0, $size = 10, $defaultColor = '000')
 	{
 		if($size <= 5)
-			self::onImageQuality($string, $image, $fontName, $x, $y, $size,
-				$defaultColor, 3);
+			self::onImageQuality($string, $image, $fontName, $x, $y, $size, $defaultColor, 3);
 		else if($size <= 10)
-			self::onImageQuality($string, $image, $fontName, $x, $y, $size,
-				$defaultColor, 2);
+			self::onImageQuality($string, $image, $fontName, $x, $y, $size, $defaultColor, 2);
 		else if($size <= 20)
-			self::onImageQuality($string, $image, $fontName, $x, $y, $size,
-				$defaultColor, 1);
+			self::onImageQuality($string, $image, $fontName, $x, $y, $size, $defaultColor, 1);
 		else
 			self::onImageFast($string, $image, $fontName, $x, $y, $size, $defaultColor);
 	}
@@ -159,15 +152,13 @@ class StyleParser
 	 * @param int $size	Font size
 	 * @param string $defaultColor Default text color in 3 or 6 hexadecimal characters
 	 */
-	static function onImageFast($string, $image, $fontName, $x=0, $y=0, $size=10,
-		$defaultColor='000')
+	static function onImageFast($string, $image, $fontName, $x = 0, $y = 0, $size = 10, $defaultColor = '000')
 	{
 		$defaultColor = Color::StringToRgb24($defaultColor);
 
 		$xOffset = 0;
 		foreach(self::parseString($string) as $token)
-			$xOffset += $token->onImage($image, $fontName, $x + $xOffset, $y, $size,
-				$defaultColor, 1);
+			$xOffset += $token->onImage($image, $fontName, $x + $xOffset, $y, $size, $defaultColor, 1);
 	}
 
 	/**
@@ -181,8 +172,7 @@ class StyleParser
 	 * @param string $defaultColor Default text color in 3 or 6 hexadecimal characters
 	 * @param int $precision Higher value means higher precision (but also longer execution time), usual values are 1 to 3
 	 */
-	static function onImageQuality($string, $image, $fontName, $x=0, $y=0,
-		$size=10, $defaultColor='000', $precision=2)
+	static function onImageQuality($string, $image, $fontName, $x = 0, $y = 0, $size = 10, $defaultColor = '000', $precision = 2)
 	{
 		$defaultColor = Color::StringToRgb24($defaultColor);
 		$factor = 1 << ($precision > 31 ? 31 : $precision);
@@ -195,32 +185,29 @@ class StyleParser
 
 		$brush = imagecreatetruecolor($maxBBox[2] * 2, -$maxBBox[5] + 5);
 		imagefill($brush, 0, 0, 0x7fffffff);
-		$hugeBrush = imagecreatetruecolor(imagesx($brush) * $factor,
-			imagesy($brush) * $factor);
+		$hugeBrush = imagecreatetruecolor(imagesx($brush) * $factor, imagesy($brush) * $factor);
 		imagefill($hugeBrush, 0, 0, 0x7fffffff);
 
 		$xOffset = 0;
 		foreach($tokens as $token)
-			$xOffset += $token->onImage($hugeBrush, $fontName, $xOffset,
-				imagesy($hugeBrush) * .75, $factor * $size, $defaultColor, $factor);
+			$xOffset += $token->onImage($hugeBrush, $fontName, $xOffset, imagesy($hugeBrush) * .75, $factor * $size, $defaultColor, $factor);
 
 		$brushX = $x + imagesx($brush) / 2;
 		$brushY = $y - imagesy($brush) * .25;
-		imagecopyresampled($brush, $hugeBrush, 0, 0, 0, 0, imagesx($brush),
-			imagesy($brush), imagesx($hugeBrush), imagesy($hugeBrush));
+		imagecopyresampled($brush, $hugeBrush, 0, 0, 0, 0, imagesx($brush), imagesy($brush), imagesx($hugeBrush), imagesy($hugeBrush));
 		imagesetbrush($image, $brush);
 		imageline($image, $brushX, $brushY, $brushX, $brushY, IMG_COLOR_BRUSHED);
 	}
 
 	static private function parseString($string)
 	{
-		$rawTokens = preg_split('/(\$(?:[0-9a-f][^\$]{0,2}|[lhp](?:\[.*?\])?|.))/iu',
-			$string, null, PREG_SPLIT_DELIM_CAPTURE);
+		$rawTokens = preg_split('/(\$(?:[0-9a-f][^\$]{0,2}|[lhp](?:\[.*?\])?|.))/iu', $string, null, PREG_SPLIT_DELIM_CAPTURE);
 
 		$stylesStack = array();
 		$style = 0;
 		$capitalized = false;
 		$linkToken = null;
+		$linkStartLevel = 0;
 		$isManialink = false;
 		$link = null;
 
@@ -321,6 +308,7 @@ class StyleParser
 								$newToken = new TextToken($style);
 							}
 							$tokens[] = $linkToken = new LinkToken($link, $isManialink);
+							$linkStartLevel = count($stylesStack);
 						}
 
 						$isManialink = false;
@@ -335,7 +323,29 @@ class StyleParser
 						break;
 					case '>':
 						if(!empty($stylesStack))
+						{
 							$style = array_pop($stylesStack);
+							
+							if($linkToken && $linkStartLevel > count($stylesStack))
+							{
+								if($newToken->text !== '')
+								{
+									$tokens[] = $newToken;
+									if(!$linkToken->locked)
+										$linkToken->link .= $newToken->text;
+									$newToken = new TextToken($style);
+								}
+								else if(end($tokens) === $linkToken)
+								{
+									array_pop($tokens);
+									$linkToken = null;
+									break;
+								}
+
+								$tokens[] = new KnilToken();
+								$linkToken = null;
+							}
+						}
 						break;
 
 					case '0': case '1': case '2': case '3': case '4':
@@ -376,14 +386,12 @@ class StyleParser
 
 		return $tokens;
 	}
-
 }
 
 abstract class Color
 {
 
-	static function Contrast($colorRgb, $backgroundRgb,
-		$contrast=StyleParser::CONTRAST_AUTO)
+	static function Contrast($colorRgb, $backgroundRgb, $contrast = StyleParser::CONTRAST_AUTO)
 	{
 		$backgroundHsv = self::Rgb24ToHsv($backgroundRgb);
 		$colorHsv = self::Rgb24ToHsv($colorRgb);
@@ -531,16 +539,14 @@ abstract class Color
 
 		return ($r << 16) + ($g << 8) + $b;
 	}
-
 }
 
 class TextToken
 {
-
 	public $style;
 	public $text;
 
-	function __construct($style=0, $text='')
+	function __construct($style = 0, $text = '')
 	{
 		$this->style = $style;
 		$this->text = $text;
@@ -556,8 +562,7 @@ class TextToken
 				if(StyleParser::getBackground() !== null && StyleParser::getContrast() !== null)
 				{
 					$color = Color::Rgb12ToRgb24($this->style & 0xfff);
-					$color = Color::Contrast($color, StyleParser::getBackground(),
-							StyleParser::getContrast());
+					$color = Color::Contrast($color, StyleParser::getBackground(), StyleParser::getContrast());
 					$color = Color::Rgb24ToString($color);
 				}
 				else
@@ -574,8 +579,7 @@ class TextToken
 				$styles .= 'letter-spacing:.1em;font-size:105%;';
 			else if($this->style & StyleParser::NARROW)
 				$styles .= 'letter-spacing:-.1em;font-size:95%;';
-			return '<span style="'.$styles.'">'.htmlentities($this->text, ENT_QUOTES,
-					'UTF-8').'</span>';
+			return '<span style="'.$styles.'">'.htmlentities($this->text, ENT_QUOTES, 'UTF-8').'</span>';
 		}
 		else
 			return htmlentities($this->text, ENT_QUOTES, 'UTF-8');
@@ -583,15 +587,13 @@ class TextToken
 
 	function onImage($image, $fontName, $x, $y, $size, $color, $shadowOffset)
 	{
-		$fontFile = StyleParser::getFont($fontName,
-				$this->style & (StyleParser::BOLD | StyleParser::ITALIC));
+		$fontFile = StyleParser::getFont($fontName, $this->style & (StyleParser::BOLD | StyleParser::ITALIC));
 
 		if($this->style & StyleParser::COLORED)
 		{
 			$color = Color::Rgb12ToRgb24($this->style & 0xfff);
 			if(StyleParser::getBackground() !== null && StyleParser::getContrast() !== null)
-				$color = Color::Contrast($color, StyleParser::getBackground(),
-						StyleParser::getContrast());
+				$color = Color::Contrast($color, StyleParser::getBackground(), StyleParser::getContrast());
 		}
 
 		$width = 0;
@@ -605,8 +607,7 @@ class TextToken
 				$bBox = imagettfbbox($size, 0, $fontFile, $char);
 				//echo print_r($bBox, true).'<br>';
 
-				$temp = imagecreatetruecolor(($bBox[2] - $bBox[0]) * 2,
-					($bBox[3] - $bBox[5]) * 2);
+				$temp = imagecreatetruecolor(($bBox[2] - $bBox[0]) * 2, ($bBox[3] - $bBox[5]) * 2);
 				imagefill($temp, 0, 0, 0x7fffffff);
 				$brush = imagecreatetruecolor(imagesx($temp) * $ratio, imagesy($temp));
 				imagefill($brush, 0, 0, 0x7fffffff);
@@ -614,12 +615,9 @@ class TextToken
 				$brushY = $y;
 
 				if($this->style & StyleParser::SHADOWED)
-					imagettftext($temp, $size, 0, -$bBox[0] + $shadowOffset,
-						$bBox[3] - $bBox[5] + $shadowOffset, 0x3f000000, $fontFile, $char);
-				imagettftext($temp, $size, 0, -$bBox[0], $bBox[3] - $bBox[5], $color,
-					$fontFile, $char);
-				imagecopyresampled($brush, $temp, 0, 0, 0, 0, imagesx($brush),
-					imagesy($brush), imagesx($temp), imagesy($temp));
+					imagettftext($temp, $size, 0, -$bBox[0] + $shadowOffset, $bBox[3] - $bBox[5] + $shadowOffset, 0x3f000000, $fontFile, $char);
+				imagettftext($temp, $size, 0, -$bBox[0], $bBox[3] - $bBox[5], $color, $fontFile, $char);
+				imagecopyresampled($brush, $temp, 0, 0, 0, 0, imagesx($brush), imagesy($brush), imagesx($temp), imagesy($temp));
 				imagesetbrush($image, $brush);
 				imageline($image, $brushX, $brushY, $brushX, $brushY, IMG_COLOR_BRUSHED);
 
@@ -636,22 +634,18 @@ class TextToken
 				$bBox = imagettfbbox($size, 0, $fontFile, $char);
 
 				if($this->style & StyleParser::SHADOWED)
-					imagettftext($image, $size, 0, $x + $width - $bBox[0] + $shadowOffset,
-						$y + $shadowOffset, 0x3f000000, $fontFile, $char);
-				imagettftext($image, $size, 0, $x + $width - $bBox[0], $y, $color,
-					$fontFile, $char);
+					imagettftext($image, $size, 0, $x + $width - $bBox[0] + $shadowOffset, $y + $shadowOffset, 0x3f000000, $fontFile, $char);
+				imagettftext($image, $size, 0, $x + $width - $bBox[0], $y, $color, $fontFile, $char);
 
 				$width += $bBox[2] - $bBox[0] + $extraSpace;
 			}
 		}
 		return $width;
 	}
-
 }
 
 class LinkToken
 {
-
 	public $link;
 	public $isManialink;
 	public $locked;
@@ -687,7 +681,6 @@ class LinkToken
 	{
 		return null;
 	}
-
 }
 
 class KnilToken
@@ -702,7 +695,6 @@ class KnilToken
 	{
 		return null;
 	}
-
 }
 
 ?>
