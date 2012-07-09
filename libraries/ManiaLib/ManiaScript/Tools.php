7671fde8b03c8012114bef834a21c8b43832a8c7
gou1 <maximeraoust@gmail.com>
2012-07-09 12:59:04 +0000

    Updated ManiaLib\ManiaScript\Tools::array2maniascript  * Added optional param to preserve keys  * Handles recursion
    git-svn-id: http://manialib.googlecode.com/svn/branches/3.0-wip@729 e79368ca-600c-11de-ad4a-3d330faba797

--- libraries/ManiaLib/ManiaScript/Tools.php
+++ libraries/ManiaLib/ManiaScript/Tools.php
@@ -39,20 +39,24 @@ abstract class Tools
 	 * Converts a PHP array to a Text[] ManiaScript array (note the type!).
 	 * Usefull when used with "ManiaScript Framework Actions"
 	 */
-	static function array2maniascript(array $array)
+	static function array2maniascript(array $array, $preserveKeys = false)
 	{
-		if(!$array)
+		foreach($array as $k => $v)
 		{
-			// hack because "[]" is not supported and "Text[]" doesnt work yet
-			return '[""]';
-		}
-		$array = array_values($array);
-		$array = array_map(function($element)
+			if(is_array($v))
 			{
-				return '"'.Tools::escapeString($element).'"';
-			}, array_values($array));
-		$array = '['.implode(',', $array).']';
-		return $array;
+				$array[$k] = self::array2maniascript($v, $preserveKeys);
+			}
+			elseif($preserveKeys)
+			{
+				$array[$k] = sprintf('"%s"=>"%s"', self::escapeString($k), self::escapeString($v));
+			}
+			else
+			{
+				$array[$k] = sprintf('"%s"', self::escapeString($v));
+			}
+		}
+		return '['.implode(', ', $array).']';
 	}
 
 }
